18f51c1173779ce8d5064dfcd75ab685ff240dfe
Jaeyeon Lee <Jaeyeon.Lee@bcm.edu>
2024-08-22 09:22:08 -0500

    Limit s3 bucket one of two

--- utils/download_hgmd.sh
+++ utils/download_hgmd.sh
@@ -10,6 +10,13 @@ fi
 DATA_DIR_PREFIX=$1
 S3_BUCKET_ID=$2
 
+# Validate the S3 bucket ID
+if [[ "$S3_BUCKET_ID" != "aim-data-dependencies-2.0" && "$S3_BUCKET_ID" != "aim-data-dependencies-2.0-public" ]]; then
+    echo "Error: S3 bucket ID must be either 'aim-data-dependencies-2.0' or 'aim-data-dependencies-2.0-public'."
+    exit 1
+fi
+
+
 # Define the list of files to download
 FILES=(
     "merge_expand/hg19/hgmd_c.tsv.gz"
@@ -29,14 +36,14 @@ for FILE in "${FILES[@]}"; do
     # Construct the full S3 path and the local file path
     S3_PATH="s3://${S3_BUCKET_ID}/${FILE}"
     LOCAL_PATH="${DATA_DIR_PREFIX}/${FILE}"
-    
+
     # Create the directory if it does not exist
     mkdir -p "$(dirname "${LOCAL_PATH}")"
-    
+
     # Download the file
     echo "Downloading ${S3_PATH} to ${LOCAL_PATH}..."
     aws s3 cp "${S3_PATH}" "${LOCAL_PATH}"
-    
+
     if [ $? -ne 0 ]; then
         echo "Failed to download ${S3_PATH}"
         exit 1
