8b46f43274a85085e24bd35ccfc655d0ded89c4c
Zhijian Yu <yuzhijian@outlook.com>
2024-09-25 17:09:28 -0500

    parse and organize the genemap2.txt file

--- utils/hpo_update/hpo_update.r
+++ utils/hpo_update/hpo_update.r
@@ -1,3 +1,5 @@
+#NOTE: Replace the download links with newest ones and then parse the files
+
 library(tidyverse)
 library(httr)
 
@@ -11,7 +13,9 @@ GET(hpo_omim_file, write_disk("phenotype.hpoa", overwrite = TRUE))
 GET(genemap2_file, write_disk("genemap2.txt", overwrite = TRUE))
 
 
-#use old dataframe for reference; read the new data file
+#read the new data file
+df_new <- read_delim("phenotype.hpoa", delim = '\t', skip = 4)
+
 old_colnames <- c("OMIM_ID",
                   "HPO_ID",
                   "DiseaseName",
@@ -19,7 +23,6 @@ old_colnames <- c("OMIM_ID",
                   "Frequency",
                   "Sex",
                   "Aspect")
-df_new <- read_delim("phenotype.hpoa", delim = '\t', skip = 4)
 
 #make the format of new data file same as the old one
 df_new_omim <- df_new %>%
@@ -37,8 +40,44 @@ df_new_omim_clean <- df_new_omim %>%
 colnames(df_new_omim_clean) <- old_colnames
 df_new_omim_clean$OMIM_ID <- as.numeric(df_new_omim_clean$OMIM_ID)
 
-#remove unnecessary file
-file.remove("phenotype.hpoa")
-
 #save the new HPO_OMIM.tsv
 write_tsv(df_new_omim_clean, 'HPO_OMIM.tsv')
+
+#parse the genemap2 table
+system("cat genemap2.txt | ./parseGeneMap2_output.py > genemap2_parsed.txt",
+       intern = FALSE)
+
+#organize the table
+old_colnames <- c(
+  "MIM_Number",
+  "Pheno_ID",
+  "Phenotypes",
+  "Approved_Gene_Symbol",
+  "Gene_Symbols",
+  "Ensembl_Gene_ID",
+  "Entrez_Gene_ID",
+  "inheritance",
+  "Pheno_mapKey"
+)
+
+gm2_new_parsed <- read_delim('genemap2_parsed.txt')
+gm2_new_parsed_selected <- gm2_new_parsed |>
+  select(
+    MIM_Number,
+    Phenotype_MIM_Number,
+    Phenotype_Name,
+    Approved_Gene_Symbol,
+    Gene_Symbols,
+    Ensembl_Gene_ID,
+    Entrez_Gene_ID,
+    Inheritance,
+    Mapping_Key
+  )
+colnames(gm2_new_parsed_selected) <- old_colnames
+
+#save genemap2 data as rds
+#the file name here is not changed, even though the newest data is from 2024
+saveRDS(gm2_new_parsed_selected, file = "genemap2_v2022.rds")
+
+#remove unnecessary file
+file.remove("phenotype.hpoa", "genemap2.txt", "genemap2_parsed.txt")
